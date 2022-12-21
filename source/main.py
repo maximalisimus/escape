b402727b428b285ca574726c0d9810eda7593a80
maximalis171091@yandex.ru <maximalis171091@yandex.ru>
2022-12-21 08:58:05 +0300

    Update

--- source/main.py
+++ source/main.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import pathlib
+
+images = {'bonus': {
+					
+					},
+		'wall': {
+				
+				},
+		'bg': {
+				
+				}
+		}
+
+levels = {
+			
+		}
+
+sounds = {
+			
+		}
+
 def main():
 	pass
 
