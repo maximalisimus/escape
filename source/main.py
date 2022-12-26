bb3b365f1aaf10ea4ef9663ce53c7c1d82d8c7a5
Mikl <maximalis171091@yandex.ru>
2022-12-26 22:09:30 +0300

    Update

--- source/main.py
+++ source/main.py
@@ -682,10 +682,19 @@ def BuildLevel(surface, GroupMap, level):
 	for row in lines:
 		for col in row.replace('\n', '').replace(' ','7'):
 			if LevelCode.GetCodeValue(int(col)) != LevelCode.Empty:
-				pass
+				tmp = SwitchBlockMap(LevelCode.GetCodeValue(int(col)), level)
+				if tmp[1] == TypeBlock.Door:
+					if DoorInOut:
+						Block(tmp[0], TypeBlock.DoorOut, (x, y), GroupMap)
+						DoorInOut = False
+					else:
+						Block(tmp[0], TypeBlock.DoorIn, (x, y), GroupMap)
+				else:
+					Block(tmp[0], tmp[1], (x, y), GroupMap)
 			x+=size_blocks
 		y+=size_blocks
 		x=0
+	GroupMap.draw(surface)
 
 def print_level(level: int) -> str:
 	if level<10:
@@ -890,6 +899,7 @@ def main():
 	LevelMap = pygame.sprite.Group()
 	BuildLevel(surf_table, LevelMap, 1)
 	sc.blit(surf_table, (0, 0))
+	
 			
 	#surf_start_bg = pygame.transform.scale(images['bg'][6]['surf'], (W, H))
 	#sc.blit(surf_start_bg, (0, 0))
