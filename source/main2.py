1beae47708c327affd86552545d5d3401d086d9c
Mikl <maximalis171091@yandex.ru>
2022-12-31 00:17:33 +0300

    Update

--- source/main2.py
+++ source/main2.py
@@ -17,6 +17,13 @@ pygame.display.set_icon(pygame.image.load(str(pathlib.Path('./config/').joinpath
 
 clock = pygame.time.Clock()
 
+surf_table = pygame.Surface((size_table[0], size_table[1]), pygame.SRCALPHA, 32).convert_alpha()
+rect_table = surf_table.get_rect(topleft=(0, 0))
+
+surf_score = pygame.Surface((size_surf_score[0], size_surf_score[1]), pygame.SRCALPHA, 32).convert_alpha()
+surf_level = pygame.Surface((size_surf_level[0], size_surf_level[1]), pygame.SRCALPHA, 32).convert_alpha()
+surf_lives = pygame.Surface((size_surf_lives[0], size_surf_lives[1]), pygame.SRCALPHA, 32).convert_alpha()
+
 def main():
 	RUN = True
 	while RUN:
